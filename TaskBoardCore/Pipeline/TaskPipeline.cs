ac4cb6f1a84bccd69356e9919e7f20bcd9c7a481
Демин Дмитрий Владимирович <demindmytry@work.kodeks.ru>
2017-06-20 15:54:42 +0300

    third commit

---
+++ TaskBoardCore/Pipeline/TaskPipeline.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBoardCore.Pipeline
+{
+    public class TaskPipeline
+    {
+        private const int defaultPipelineCapacity = 5;
+        private readonly HyperlinkerTask [] _pipeline;
+
+        public TaskPipeline() : this( defaultPipelineCapacity ) { }
+
+        public TaskPipeline( int pipelineCapacity )
+        {
+            _pipeline = new HyperlinkerTask [ pipelineCapacity ];
+        }
+
+        //public bool Add( HyperlinkerTask task )
+        //{
+
+        //}
+    }
+}
